0bd136a122b31015eb72360b73ad4d5837861c4e
AngelR000 <angel01680168@gmail.com>
2022-04-11 23:42:23 -0500

    acceso guest

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('guest');
 
 
 Route::get('/kardex', [App\Http\Controllers\PdfController::class, 'kardex'])->name('kardex');
