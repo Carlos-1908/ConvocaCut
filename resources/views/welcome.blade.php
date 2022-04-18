8beab42d20c73a72e4e7979c70291e0baa17e0a1
AngelR000 <angel01680168@gmail.com>
2022-04-17 19:33:45 -0500

    rol

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,6 @@
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
                     @endauth
-                    <a href="{{ url('/kardex') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Kardex</a>
                 </div>
             @endif
 
